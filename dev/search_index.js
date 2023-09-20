0b23c5fc4bec26b50fd4440b3d5ccee256ef40fd
Documenter.jl <documenter@juliadocs.github.io>
2023-09-20 07:23:56 +0000

    build based on 0b99b79

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = memoProgClass2023w","category":"page"},{"location":"#memoProgClass2023w","page":"Home","title":"memoProgClass2023w","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for memoProgClass2023w.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [memoProgClass2023w]","category":"page"}]
+}
